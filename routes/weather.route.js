fbcde5dcd8adabcb02f02909c8d0e591169e53f8
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-01-03 22:37:47 +0530

    added

---
+++ routes/weather.route.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const apiKey = process.env.API_KEY;
+
+router.post('/getWeather', async (req, res) => {
+    try {
+        const { cities } = req.body;
+        if (!cities || !Array.isArray(cities)) {
+            return res.status(400).json({ error: 'Invalid input. Please provide an array of cities.' });
+        }
+
+        const weatherData = {};
+        const requests = cities.map(async (city) => {
+            const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            if (response.ok) {
+                const temperature = `${data.main.temp}°C`;
+                weatherData[city] = temperature;
+            } else {
+                weatherData[city] = 'Not available';
+            }
+        });
+
+        await Promise.all(requests);
+        return res.status(200).json({ weather: weatherData });
+    } catch (error) {
+        return res.status(500).json({ error: 'Something went wrong.' });
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+const weatherRoute = require('./routes/weather.route');
 
 const app = express();
+dotenv.config();
 
+app.use('/api',weatherRoute);
 
 app.listen(8080,()=>{
 
